4ac49445175abc59b9078e3f8ce77101adac1bb2
MeowMerry <meow3525@gmail.com>
2022-08-11 06:12:31 -0500

    create react-emoji searh app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import { emojiData } from './emoji';
+import { TextField } from '@mui/material';
+interface EmojiDataProp {
+  title: string;
+  symbol: string;
+  keywords: string;
+}
 function App() {
+  const [search, setSearch] = useState<any>();
+  const [data, setData] = useState<EmojiDataProp[]>([]);
+
+  useEffect(() => {
+    const newData = emojiData.filter(emoji => emoji.title.toLocaleLowerCase().includes(search?.toLocaleLowerCase()))
+    setData(newData)
+  }, [search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-header'>
+        <label htmlFor="input">Emoji Search</label>
+      </div>
+      <div >
+        <TextField type="text" value={search}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} />
+      </div>
+      <div className='emaji-box'>
+        {data.map(emoji => (
+          <div className='emaji-display' onClick={() => { navigator.clipboard.writeText(emoji.symbol); alert("Emoji Copy") }} key={`emoji` + emoji.title}>
+            <span>{emoji.symbol}</span> <span>{emoji.title}</span></div>
+        ))}
+      </div>
     </div>
   );
 }
